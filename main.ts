13fc306143fdd6bea05fbf2d27328a43b3a44fbc
rrabit243 <88839555+rrabit243@users.noreply.github.com>
2021-08-27 08:26:55 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    led.plot(0, 2)
+    led.plot(2, 0)
+    led.plot(4, 2)
+    led.plot(1, 2)
+    led.plot(2, 2)
+    led.plot(3, 2)
+    led.plot(3, 1)
+    led.plot(1, 1)
+})
